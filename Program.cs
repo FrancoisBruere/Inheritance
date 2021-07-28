58668766f54b5808811d4f115989da8e09ff9f79
FrancoisBruere <85226595+FrancoisBruere@users.noreply.github.com>
2021-07-28 09:15:37 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Inheritance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var text = new Text();
+
+            text.Width = 100;
+            text.Copy();
+
+
+
+        }
+    }
+}
